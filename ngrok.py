ff3b95c06efc80d455b3a8033d9c02821ff52379
Umesh <43033939+umesh11111@users.noreply.github.com>
2026-03-03 18:07:32 +0530

    Delete ngrok.py

--- ngrok.py
+++
@@ -1,7 +0,0 @@
-from pyngrok import ngrok
-import time
-
-ngrok.set_auth_token("3AO269koMWSJ1iypM1kJUNJG4Ke_3AFg2eTYhRp3JYMJb9XaB")
-http_tunnel = ngrok.connect(3000)
-print (http_tunnel.public_url)
-time.sleep(30000)
